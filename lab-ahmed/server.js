817add7c92cb692615750988e44be9144bb11875
Ahmed Ali <Ahmed98148@gmail.com>
2018-03-04 12:54:07 -0800

    refatcor

--- lab-ahmed/server.js
+++ lab-ahmed/server.js
@@ -3,60 +3,12 @@
 const http = require('http');
 const Car = require('./model/car.js');
 const Router = require('./lib/router.js');
+const carRouter = require('./route/car-route.js');
 const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
-router.get('/api/car', function(req, res) {
-  if (req.url.query.id) {
-    storage.fetchItem('car', req.url.query.id)
-    .then( car => {
-      res.writeHead(200, {
-        'Content-Type': 'text/plain'
-      });
-
-      res.write(JSON.stringify(car));
-      res.end();
-    })
-    .catch( err => {
-      console.error(err);
-      res.writeHead(404, {
-        'Content-Type': 'text/plain'
-      });
-
-      res.write('route not found');
-      res.end();
-    });
-
-    return;
-  }
-
-  res.writeHead(400, {
-    'Content-Type': 'text/plain'
-  });
-
-  res.write('bad request');
-  res.end();
-});
-
-router.post('/api/car', function(req, res) {
-  try {
-    var car = new Car(req.body.name, req.body.content);
-    storage.createItem('car', car);
-    res.writeHead(200, {
-      'Content-Type': 'text/plain'
-    });
-    res.write(JSON.stringify(car));
-    res.end();
-  } catch (err) {
-    console.error(err);
-    res.writeHead(400, {
-      'Content-Type': 'text/plain'
-    });
-    res.write('bad request');
-    res.end();
-  }
-});
+carRouter(router);
 
 const server = http.createServer(router.route());
 
